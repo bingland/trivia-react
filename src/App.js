de1a8b13a0cabd01055e9e451e20620c51c09912
Brayden England <bray.patriot@gmail.com>
2020-09-29 22:59:50 -0600

    SCSS implemented, Axios displays info

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+import './scss/styles.scss'
+import Startup from './Startup';
 
 function App() {
+
+  const [questions, setQuestions] = useState({});
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getQuestions = () => {
+    axios.get(`https://opentdb.com/api.php?amount=10&category=23&difficulty=medium&type=multiple`)
+      .then(res => {
+        const data = res.data;
+        setQuestions(data.results)
+        setIsLoading(false)
+      })
+  }
+  
+  // initial useEffect
+  useEffect(() => {
+    getQuestions()
+  }, [])
+
+  let questionList
+  if (!isLoading) {
+    questionList = questions.map((item, index) => {
+      return <li key={index}>{item.question}</li> 
+    })
+  } else {
+    questionList = <div>Loading....</div>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="green">Trivia App</h1>
+      <Startup />
+      <ul>
+        {questionList}
+      </ul>
     </div>
   );
 }
---
+++ src/Startup.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Startup = () => {
+    return (
+        <div>Hi lol</div>
+    )
+}
+
+export default Startup
